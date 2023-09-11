628c97fd130c8b93731a335a4c43b4e97f122577
Khyn Antoque <khynantoque@gmail.com>
2023-09-11 09:04:15 +0800

    Finished: First Release

--- src/constants/index.js
+++ src/constants/index.js
@@ -134,51 +134,51 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
+      "I thought it was impossible to make a website as beautiful as I expected, but Khyn proved me wrong.",
+    name: "Chan Sucgang",
+    designation: "Freelancer",
+    company: "",
+    image: "https://scontent.fceb1-2.fna.fbcdn.net/v/t39.30808-6/326530443_886578422537458_6365792971641025779_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=1b51e3&_nc_eui2=AeETpBDoPtg0RhIMo4sQGHkGzaju83Yh-qrNqO7zdiH6qn0W9hNY8jzDfWOI00FGViEmShWG_Ga1l80gvU77myfy&_nc_ohc=KJusbrJJkGoAX9D77xZ&_nc_ht=scontent.fceb1-2.fna&oh=00_AfB2_bvpevqfcpdsFcbpkfrhibXdvuup-tkSif7wGJJnkQ&oe=65039A7C",
   },
   {
     testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
+      "I've never met a web developer who truly cares about their clients' success like Khyn does.",
+    name: "Jimrie Perez",
+    designation: "Freelancer",
+    company: "",
+    image: "https://scontent.fcgy2-1.fna.fbcdn.net/v/t39.30808-6/358526493_2011991002483379_2829139894856747080_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=1b51e3&_nc_eui2=AeGbaN0sFdWuakC6vVJFxGhEwUeMhy6wPDLBR4yHLrA8MoAdz_9j0dHHfFCM8MjeKdLmCLbElHd2B0Ji-Dk8TxT5&_nc_ohc=kqb5vTK6_oIAX-Ah6VC&_nc_ht=scontent.fcgy2-1.fna&oh=00_AfCglsSyYaI7M8IEBozFn8qJO0T_acnGghzXP1NLqv6B4Q&oe=650298E6",
   },
   {
     testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
+      "After Khyn optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "",
+    designation: "Tech Support",
+    company: "| VXI",
+    image: "https://scontent.fceb1-2.fna.fbcdn.net/v/t39.30808-6/351159752_569841295301207_6200153810540878642_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=a2f6c7&_nc_eui2=AeH918h2Ww_SgqD9qJX_gHcVjnhPBem3ewmOeE8F6bd7Cesa3zWqEo1o7thkgwMTGxEU5WTVuMgCEFEUBzCWa45n&_nc_ohc=XvE10y_gMMsAX8ccQD4&_nc_ht=scontent.fceb1-2.fna&oh=00_AfBBXH7TUF5YNBY6xZkxsJD-d0NBU6x_knFZwo6hQxWcFg&oe=6503FD32",
   },
 ];
 
 const projects = [
   {
-    name: "Car Rent",
+    name: "Joblessed",
     description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
     tags: [
       {
         name: "react",
         color: "blue-text-gradient",
       },
       {
-        name: "mongodb",
+        name: "django-rest",
         color: "green-text-gradient",
       },
       {
-        name: "tailwind",
+        name: "scss",
         color: "pink-text-gradient",
       },
     ],
-    image: carrent,
-    source_code_link: "https://github.com/",
+    image: jobit,
+    source_code_link: "https://github.com/khynantoque/Joblessed",
   },
 ];
 
